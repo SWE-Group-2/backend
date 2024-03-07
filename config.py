5bf78d7e830e0c820af360372a745e34f92ae260
mythicalbadger <jussy12.work@gmail.com>
2024-03-07 08:23:41 +0700

    feat: config for sqlalchemy

--- config.py
+++ config.py
@@ -6,4 +6,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config:
     """Application configuration."""
 
-    pass
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+        "DATABASE_URI"
+    ) or "sqlite:///" + os.path.join(basedir, "app.db")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
