b534df44731e54e8c9137db65d2ee2eaa72c56c8
mythicalbadger <jussy12.work@gmail.com>
2024-03-19 15:04:26 +0700

    feat: role enum

--- src/app/models/roles.py
+++ src/app/models/roles.py
@@ -1,4 +1,18 @@
+import enum
+
 from src.app.extensions import db
+
+
+class RoleEnum(enum.Enum):
+    """Enum for the role of a User."""
+
+    admin = 1
+    instructor = 2
+    student = 3
+
+    def __repr__(self) -> str:
+        """Return a string representation of the RoleEnum."""
+        return f"<RoleEnum '{self.value}'>"
 
 
 class Roles(db.Model):
