f62f4d8df0d57d1bcfb0d6766205fd957976d104
mythicalbadger <jussy12.work@gmail.com>
2024-03-20 18:27:45 +0700

    feat: tests for get user by id

--- tests/conftest.py
+++ tests/conftest.py
@@ -27,6 +27,7 @@ class EndpointEnum(enum.Enum):
 
     login = "/users/login"
     get_current_user = "/users/get_current_user"
+    get_user = "/users/{user_id}"
     register = "/users/register"
 
 
