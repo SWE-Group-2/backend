7c2d8ce8757291da408e71744ae7ab36a8772350
louisbank-edu-thailand <louis.ban@student.mahidol.edu>
2024-03-25 21:45:04 +0700

    added functionality and tests for task

--- src/app/services/admin_service.py
+++ src/app/services/admin_service.py
@@ -11,3 +11,10 @@ class AdminService:
         user = Users.query.filter_by(id=user_id).first()
         db.session.delete(user)
         db.session.commit()
+
+    @staticmethod
+    def change_user_role_by_id(user_id: int, role_id: int) -> None:
+        """Change a user's role by id."""
+        user = Users.query.filter_by(id=user_id).first()
+        user.role_id = role_id
+        db.session.commit()
