7f69ccfa03c842420818a0f99939952c11ff8d5f
mythicalbadger <jussy12.work@gmail.com>
2024-03-28 09:13:14 +0700

    feat: constants for response messages

---
+++ src/app/constants/error_messages.py
@@ -0,0 +1,8 @@
+"""Module for error messages returned in route responses."""
+
+USER_NOT_FOUND = "User not found"
+INVALID_PASSWORD = "Invalid password"
+USERNAME_EXISTS = "Username already exists"
+
+INVALID_REQUEST_BODY = "Invalid request body"
+MISSING_REQUIRED_DATA = "Missing data for required field."
---
+++ src/app/constants/success_messages.py
@@ -0,0 +1,4 @@
+"""Module for success messages returned in route responses."""
+
+USER_CREATED = "User created successfully"
+USER_UPDATED = "User profile updated successfully"
